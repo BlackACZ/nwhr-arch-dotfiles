53e6d534524b86a3f6c03eaea7721f653dccda8b
Antonio Atonal <uwuacz@gmail.com>
2026-02-18 23:07:51 -0600

    Configure 'ly' display manager in dm.sh
    Updated the dm.sh script to configure the 'ly' display manager with a backup of the original config.

--- scripts/dm.sh
+++ scripts/dm.sh
@@ -8,7 +8,14 @@ install_dm() {
     )
     install_packages "display manager" "${packages[@]}"
 
-    echo -e "${cyan}==> enabling ly...${nc}"
+    echo -e "${cyan}==> configuring ly...${nc}"
+
+    # Backup original config
+    [ -f /etc/ly/config.ini ] && cp /etc/ly/config.ini /etc/ly/config.ini.bak
+
+    # Copy our config
+    cp "$DOTFILES_DIR/.config/ly/config.ini" /etc/ly/config.ini
+
     systemctl enable ly.service
     systemctl disable getty@tty2.service 2>/dev/null
 
